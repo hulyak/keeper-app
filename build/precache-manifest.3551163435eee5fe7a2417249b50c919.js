61f125cda96ff85fedf551ce7099d57a312840c7
hulyak <57311842+hulya95@users.noreply.github.com>
2020-06-23 14:43:45 -0700

    change background

---
+++ build/precache-manifest.3551163435eee5fe7a2417249b50c919.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a933042fdb3a6773e436c6657f56b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6af92ef11b3e7eee3fe8",
+    "url": "/static/js/2.58c64de3.chunk.js"
+  },
+  {
+    "revision": "9764f8757c4dfcc87354",
+    "url": "/static/js/main.f4b60d67.chunk.js"
+  },
+  {
+    "revision": "583e51a7281672351041",
+    "url": "/static/js/runtime-main.38f647cd.js"
+  }
+]);
