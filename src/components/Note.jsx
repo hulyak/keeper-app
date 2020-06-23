ef9e9656b4952ecd09af06ba7a03654cc341ae93
hulyak <57311842+hulya95@users.noreply.github.com>
2020-06-23 14:19:51 -0700

    chore:add material ui/core and icons

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
+
 // properties should go inside html not inside custom components
 function Note(props) {
   function handleClick() {
@@ -8,7 +10,9 @@ function Note(props) {
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={handleClick}>DELETE</button>
+      <button onClick={handleClick}>
+      <DeleteIcon />
+      </button>
     </div>
   );
 }
